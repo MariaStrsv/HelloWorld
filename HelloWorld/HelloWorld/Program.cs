e3c9dde907aaf178abbcba67a1f025a9d27f6e28
Mary <mufu@mail.ru>
2016-08-19 09:12:33 +0500

    Теперь здороваемся не с миром, а пользователем. И знакомимся с ним

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,14 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Приятно познакомиться, " + name + "!");
+            //string age = Console.ReadLine();
+            //Console.WriteLine("How old are you?");
+            //string age = Console.ReadLine();
+
             Console.ReadLine();
         }
     }
