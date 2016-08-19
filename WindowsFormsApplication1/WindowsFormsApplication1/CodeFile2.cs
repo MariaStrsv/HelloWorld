40fa3737e4f6ff3d5f21cad9ee015c4582bb9ecb
Mary <mufu@mail.ru>
2016-08-19 10:30:19 +0500

    Добавила класс CF2

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/CodeFile2.cs
@@ -0,0 +1,10 @@
+﻿namespace CF2
+{
+    public static class CF2class
+    {
+        public static int f(int x)
+        {
+            return x * 3;
+        }
+    }
+}
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CF1;
+using CF2;
 
 namespace WindowsFormsApplication1
 {
@@ -21,6 +22,11 @@ namespace WindowsFormsApplication1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show(CF1class.x2(Convert.ToInt32(textBox1.Text)).ToString());//
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(CF2class.f(Convert.ToInt32(textBox1.Text)).ToString());//
         }
     }
 }
